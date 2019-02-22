4d3edc4bf0c92cff727c79c172e1abf423a03558
jdshap <jdshap@comcast.net>
2019-02-22 18:41:23 -0500

    Add DELETE to Leaderboard widget

--- leaderboard.js
+++ leaderboard.js
@@ -1,26 +1,38 @@
 var board = [];
+var currentSelection;
 
 const scoreboard = document.getElementById("scores");
 
 lb_getData();
 
 function lb_getData() {
-	fetch('http://jdshap.github.io/leaderboard', {method: "GET"}).then(response => response.json()).then(json => json.sort(scoresort)).then(sorted => {
+	return fetch('http://jdshap.github.io/leaderboard', {method: "GET"}).then(response => response.json()).then(json => json.sort(scoresort)).then(sorted => {
 		scoreboard.innerHTML = ``;
 		sorted.slice(0, 10).forEach(score => {
-			scoreboard.innerHTML += `<div>${score.name} <span class="score">${score.score}</span></div>`;
+			scoreboard.innerHTML += `<div class="player" id="pl${score.id}">${score.name} <span class="score">${score.score}</span></div>`;
+		});
+		
+		var players = document.getElementsByClassName("player");
+		[...players].forEach(player => {
+			player.addEventListener("click", e => {selectItem(e.target)});
 		});
 	});
 }
 
-function lb_postData() {
-	fetch('http://jdshap.github.io/leaderboard', {method: "POST", body: {name: faker.internet.userName(), score: faker.random.number({min:0, max:8000})}});
-	lb_getData();
+function selectItem(element) {
+	currentSelection=parseInt(element.id.substr(2));
 }
 
-function lb_deleteData(id) {
-	fetch('http://jdshap.github.io/leaderboard', {method: "DELETE", body: {id: id}});
-	lb_getData();
+function lb_postData() {
+	fetch('http://jdshap.github.io/leaderboard', {method: "POST", body: {name: faker.internet.userName(), score: faker.random.number({min:0, max:8000})}})
+	.then(lb_getData());
+}
+
+function lb_deleteData() {
+	if(currentSelection !== undefined) {
+		fetch('http://jdshap.github.io/leaderboard', {method: "DELETE", body: {id: currentSelection}})
+		.then(lb_getData());
+	}
 }
 
 function scoresort(a,b) {
